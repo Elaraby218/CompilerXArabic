3ff35de34f1709e35c6b9519a32e15f460dd4267
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-12 05:28:00 +0300

    Added Token Class and Added Token Type (Open racket/CloseBracket)

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,15 @@
 import re
 
+
+class Token:
+    def __init__(self, token_type, token_value, line=None):
+        self.type = token_type
+        self.value = token_value
+        self.line = line
+
+    def __str__(self):
+        return f"{self.type}: {self.value} at line {self.line}"
+
 class Tokenizer:
     def __init__(self):
         self.token_patterns = {
@@ -8,6 +18,8 @@ class Tokenizer:
             'relOp': r'==|=!|=<|=>|<|>|=',
             'addOp': r'\+|-',
             'mulOp': r'\*|\\',
+            'openBracket': r'[\(\[\{]',
+            'closeBracket': r'[\)\]\}]',
             'ID': r'[اإبتثجحخدذرزسشصضطظعغفقكلمنهوى]+[\da-zA-Z]*|[\da-zA-Z]+[اإبتثجحخدذرزسشصضطظعغفقكلمنهوى]+',
             'Num': r'(-?\d+(?:,\d+)?|-?\d*\.\d+)',
             'semicolon': r'؛',
@@ -15,8 +27,11 @@ class Tokenizer:
 
     def tokenize(self, text):
         output_tokens = []
-        for match in re.finditer('|'.join('(?P<%s>%s)' % pair for pair in self.token_patterns.items()), text):
-            token_type = match.lastgroup
-            token_value = match.group(token_type)
-            output_tokens.append((token_type, token_value.strip()))
+        # split on  \n to get the line number
+        lines = text.split('\n')
+        for i, line in enumerate(lines):
+            for match in re.finditer('|'.join('(?P<%s>%s)' % pair for pair in self.token_patterns.items()), line):
+                token_type = match.lastgroup
+                token_value = match.group(token_type)
+                output_tokens.append(Token(token_type, token_value, i + 1))
         return output_tokens
