5aad2bb89026bc002294d189d0f39e7cd721e050
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-05-13 21:04:27 +0300

    Last Parser Codes

--- parser.py
+++ parser.py
@@ -62,6 +62,7 @@ class Parser:
                 self.function_declaration()
             else:
                 self.var_declaration()
+        self.reco_stmt()
 
     def function_declaration(self):
         self.parameter_list()
@@ -80,25 +81,113 @@ class Parser:
         if self.current_token.is_token("if_stmt"):
             self.match("if_stmt")
             self.match("(")
-            #self.reco_stmt() need to implement expresion stmt
+            # self.reco_stmt() need to implement expresion stmt
             self.match(")")
             self.match("{")
-            self.reco_stmt()  # need some handling in the reco_stmt function
+            self.reco_stmt()
             self.match("}")
 
     def else_condition(self):
         if self.current_token.is_token("else_stmt"):
             self.match("else_stmt")
             self.match("{")
-            #self.reco_stmt()
+            self.reco_stmt()
             self.match("}")
+
+    def iteration_stmt(self):
+        if self.current_token.is_token("iteration"):
+            self.match("iteration")
+            self.match("(")
+            # we need to match expression here
+            self.match(")")
+            self.match("{")
+            # reco
+            self.match("}")
+
+    def return_stmt(self):
+        if self.current_token.is_token("return"):
+            self.match("return")
+            if self.current_token.is_token("semicolon"):
+                self.match("semicolon")
+            else:
+                self.expression()
+                self.match("semicolon")  # second error
+
+    def expression(self):
+        if self.current_token.is_token("ID"):
+            self.var()
+            if self.current_token.is_token("="):
+                self.match("=")
+                if self.current_token.is_token("ID"):
+                    self.expression()
+        else:
+            self.simple_expression()
+
+    def var(self):
+        if self.current_token.is_token("ID"):
+            self.match("ID")
+            if self.current_token.is_token("["):
+                self.match("[")
+                self.expression()
+                self.match("]")  # first error
+        elif self.current_token.is_token("["):
+            self.match("[")
+            self.expression()
+            self.match("]")
+
+    def simple_expression(self):
+        self.additive_expression()
+        if self.current_token.is_token("relOp"):
+            self.match("relOp")
+            self.additive_expression()
+
+    def additive_expression(self):
+        self.term()
+        if self.current_token.is_token("addOp"):
+            self.match("addOp")
+            self.additive_expression()
+
+    def term(self):
+        self.factor()
+        if self.current_token.is_token("mulOp"):
+            self.match("mulOp")
+            self.term()
+
+    def factor(self):
+        if self.current_token.is_token("("):
+            self.match("(")
+            self.expression()
+            self.match(")")
+        elif self.current_token.is_token("Num"):
+            self.match("Num")
+        elif self.current_token.is_token("ID"):
+            self.match("ID")
+            if self.current_token.is_token("("):
+                self.call()
+            else:
+                self.var()
+
+    def call(self):
+        if self.current_token.is_token("ID"):
+            self.match("ID")
+            self.match("(")
+            self.args()
+            self.match(")")
+
+    def args(self):
+        if not (self.current_token.is_token(")")):
+            self.args_list()
+
+    def args_list(self):
+        self.expression()
+        if self.current_token.is_token(","):
+            self.match(",")
+            self.args_list()
 
     def parse(self):
         if not self.tokens:
             self.errors.append("Error: No tokens to parse")
             return self.errors
-
         self.current_token = self.tokens[0]
-        while self.current_token and not self.current_token.is_token("EOF"):
-            self.reco_stmt()
+        self.return_stmt()
         return self.errors
