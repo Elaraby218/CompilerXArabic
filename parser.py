489ba52e6ba7ed58141248d18d11b68e8d1841e3
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-14 13:51:49 +0300

    - Rule of function added - reco function renamed to statement - reco function now recognizes the expression-stmt - Expression statement funciton added - rewrite expression function - rewrite Var function - fix some bugs in function Factor() - function is factor added

--- parser.py
+++ parser.py
@@ -25,6 +25,7 @@ class Parser:
         else:
             self.errors.append("Error: Invalid syntax")
 
+    # 8 - params-list -> params-list , param | param
     def parameter_list(self):
         if self.current_token.is_token("("):
             self.match("(")
@@ -46,18 +47,22 @@ class Parser:
         else:
             self.error()
 
-    def reco_stmt(self):
+    # 12 - statement -> expression-statement | compound-statement | selection-statement | iteration-statement | return-statement
+    def statement(self):
         if self.current_token.is_token("specifier_type"):
             self.declaration()
-        if self.current_token.is_token("if_stmt"):
+        elif self.current_token.is_token("if_stmt"):
             self.if_condition()
             if self.current_token.is_token("else_stmt"):
                 self.else_condition()
-        if self.current_token.is_token("iteration"):
+        elif self.current_token.is_token("iteration"):
             self.iteration_stmt()
-        if self.current_token.is_token("return"):
+        elif self.current_token.is_token("return"):
             self.return_stmt()
+        elif self.is_factor() or self.current_token.is_token("semicolon"):
+            self.expression_stmt()
 
+    # 3 - declaration -> var-declaration | function-declaration
     def declaration(self):
         if self.current_token.is_token("specifier_type"):
             self.match("specifier_type")
@@ -66,11 +71,13 @@ class Parser:
                 self.function_declaration()
             else:
                 self.var_declaration()
-        self.reco_stmt()
+        self.statement()
 
+    # 6 - function-declaration -> compound-stmt ( params ) ID specifier-type
     def function_declaration(self):
         self.parameter_list()
 
+    # 4 - var-declaration -> var-declaration | fun-declaration
     def var_declaration(self):
         if self.current_token.is_token("="):
             self.match("=")
@@ -81,6 +88,8 @@ class Parser:
             self.match("]")
         self.match("semicolon")
 
+    # 13 - statement -> selection-statement
+    # 15 - selection-statement -> if ( expression ) statement | if ( expression ) statement else statement
     def if_condition(self):
         if self.current_token.is_token("if_stmt"):
             self.match("if_stmt")
@@ -88,26 +97,32 @@ class Parser:
             self.expression()
             self.match(")")
             self.match("{")
-            self.reco_stmt()
+            self.statement()
             self.match("}")
 
+    # 13 - statement -> selection-statement
+    #      selection-statement -> else-stmt
     def else_condition(self):
         if self.current_token.is_token("else_stmt"):
             self.match("else_stmt")
             self.match("{")
-            self.reco_stmt()
+            self.statement()
             self.match("}")
 
+    # 13 - statement -> iteration-statement
+    # 16 - iteration-statement -> while ( expression ) statement
     def iteration_stmt(self):
         if self.current_token.is_token("iteration"):
             self.match("iteration")
             self.match("(")
-            # we need to match expression here
+            self.expression()
             self.match(")")
             self.match("{")
-            # reco
+            self.statement()
             self.match("}")
 
+    # 13 - statement -> return-statement
+    # 17 - return-statement -> return ; | return expression ;
     def return_stmt(self):
         if self.current_token.is_token("return"):  # what if it is not return ?!!!!
             self.match("return")
@@ -116,51 +131,57 @@ class Parser:
             else:
                 self.expression()
                 self.match("semicolon")  # second error
+                # match semicolon could be called one here
 
-    def expression(self):
-        if self.current_token.is_token("ID"):
-            self.match("ID")
-            if self.current_token.is_token("="):
-                self.match("=")
-                self.expression()
-            elif self.current_token.is_token("["):
-                self.match("[")
-                self.expression()
-                self.match("]")
-            else:
-                self.errors.append(f"Expected [ or = but found {self.current_token}")
+    # 14 - expression-stmt -> expression ; | ;
+    def expression_stmt(self):
+        if self.current_token.is_token("semicolon"):
+            self.match("semicolon")
         else:
-            if self.current_token.is_token("]"):
-                self.errors.append(f"Expected Expression but found {self.current_token}")
-            else:
-                self.simple_expression()
+            self.expression()
+            self.match("semicolon")
+        self.statement()
 
+    # 18 - expression -> var = expression | simple-expression
+    def expression(self):
+        if self.current_token.is_token("ID") and self.tokens[self.index + 1].is_token("="):
+            self.var()
+            self.match("=")
+            self.expression()
+        else:
+            self.simple_expression()
+
+    # 19 - var -> ID [ expression ] | ID
     def var(self):
         self.match("ID")
         if self.current_token.is_token("["):
             self.match("[")
             self.expression()
             self.match("]")
-            self.match("=")
+            # self.match("=")
 
+    # 20 - simple-expression -> additive-expression relop additive-expression | additive-expression
     def simple_expression(self):
         self.additive_expression()
         if self.current_token.is_token("relOp"):
             self.match("relOp")
             self.additive_expression()
 
+    # 22 - additive-expression -> term addOp additive-expression | term
     def additive_expression(self):
         self.term()
         if self.current_token.is_token("addOp"):
             self.match("addOp")
             self.additive_expression()
 
+    # 24 - term -> factor mulOp term | factor
     def term(self):
         self.factor()
         if self.current_token.is_token("mulOp"):
             self.match("mulOp")
             self.term()
 
+    # 26 - factor -> ( expression ) | Num | call | var
     def factor(self):
         if self.current_token.is_token("("):
             self.match("(")
@@ -168,13 +189,20 @@ class Parser:
             self.match(")")
         elif self.current_token.is_token("Num"):
             self.match("Num")
-        elif self.current_token.is_token("ID"):
-            self.match("ID")
-            if self.current_token.is_token("("):
-                self.call()
-            else:
-                self.var()
+        elif self.current_token.is_token("ID") and self.tokens[self.index + 1].is_token("("):
+            self.call()
+        else:
+            self.var()
 
+    # if the current token is factor then it supposed to be an expression-stmt
+    def is_factor(self):
+        return (
+                self.current_token.is_token("(") or
+                self.current_token.is_token("Num") or
+                (self.current_token.is_token("ID"))
+        )
+
+    # 28 - call -> ID ( args )
     def call(self):
         if self.current_token.is_token("ID"):
             self.match("ID")
@@ -182,10 +210,12 @@ class Parser:
             self.args()
             self.match(")")
 
+    # 28 - args -> arg-list | empty
     def args(self):
         if not (self.current_token.is_token(")")):
             self.args_list()
 
+    # 29 - arg-list -> expression , arg-list | expression
     def args_list(self):
         self.expression()
         if self.current_token.is_token(","):
@@ -197,6 +227,6 @@ class Parser:
             self.errors.append("Error: No tokens to parse")
             return self.errors
         self.current_token = self.tokens[0]
-        self.reco_stmt()
+        self.statement()
         return self.errors
 
